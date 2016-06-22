fd4f4d2cb37b312305f1aba8d5f0077016ab07d0
Rodrigo Weigert <rodrigo.neorooker@gmail.com>
2016-06-21 22:33:44 -0300

    Show time?

--- src/bdconn.py
+++ src/bdconn.py
@@ -1,7 +1,7 @@
 import psycopg2
 from PyQt5 import QtWidgets
 
-DB_CONNECT_PARAMETERS = 'dbname=projdb user=postgres password=yy09lop'
+DB_CONNECT_PARAMETERS = 'dbname=DATABASE_NAME_HERE user=POSTGRES_USER_NAME_HERE'
 
 def select(tabela, attr=None):
     """ recebe o nome da tabela e os atributos a serem selecionados
